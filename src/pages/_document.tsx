0da117e63ae2abb53c16db1426f41c35fff7bfb0
edward <3zangelks@gmail.com>
2020-10-11 22:59:34 +0800

    add kanban  loli

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"
+          />
+          <script src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js"></script>
         </body>
       </Html>
     )
